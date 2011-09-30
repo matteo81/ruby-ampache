0b33e76b833ead4b8ffc801a5f03fa583d608df4
Matteo Agostinelli <agostinelli@gmail.com>
2011-09-30 20:16:30 +0200

    Let's add some comments

--- lib/lib-ui.rb
+++ lib/lib-ui.rb
@@ -5,31 +5,45 @@ class MainWidget < Qt::Widget
 		super(parent)
 		
         self.window_title = 'Ramp'
+		# resize to a sane value
+		# TODO: load/save on startup/close
         resize(800, 400)
  
         button = Qt::PushButton.new('Quit', self) do
             connect(SIGNAL :clicked) { Qt::Application.instance.quit }
         end
 		
+		# the list widget showing the artists
         @artistList = Qt::ListView.new(self);
  
+		# initialize the layout of the main widget
         layout = Qt::VBoxLayout.new(self) do |l|
             l.add_widget(@artistList)
             l.add_widget(button)
         end
 		
+		# do some initialization
+		# using a separate thread to avoid GUI freeze
 		initialize = Thread.new do
+			# attempt to connect to Ampache server
 			initializeConnection
 			
+			# artist model (defined simply as a stringlist)
+			# see model/view programming
+			# http://doc.qt.nokia.com/latest/model-view-programming.html
 			@artistModel = Qt::StringListModel.new do |model|
 				artists = @ampache.artists
  				names = []
+				# extract the name from AmpacheArtist
+				# TODO: don't lose the uid
  				artists.each do |artist|
  					names << artist.name
  				end
+				# initialize the model
 				model.setStringList names
 			end
 			
+			# set the model to the view
 			@artistList.setModel @artistModel
 		end
     end
