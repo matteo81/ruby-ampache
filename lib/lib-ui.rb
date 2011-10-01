cf499d2a0eb6c450c389ebb366af540b1afc3f09
Matteo Agostinelli <agostinelli@gmail.com>
2011-10-01 15:19:16 +0200

    Implemented basic browsing of albums
    Clicking on an artist now yields the list of albums

--- lib/lib-ui.rb
+++ lib/lib-ui.rb
@@ -1,6 +1,7 @@
 require 'Qt4'
 
 class MainWidget < Qt::Widget
+    slots 'updateAlbums(const QModelIndex&)'
 	def initialize(parent=nil)
 		super(parent)
 		
@@ -14,11 +15,22 @@ class MainWidget < Qt::Widget
         end
 		
 		# the list widget showing the artists
-        @artistList = Qt::ListView.new(self);
+        @artistListView = Qt::ListView.new(self)
+        
+        Qt::Object.connect( @artistListView, SIGNAL('clicked(const QModelIndex&)'),
+                  self, SLOT( 'updateAlbums(const QModelIndex&)' ) )
+        @albumListView = Qt::ListView.new(self);
  
+        # artist model (defined simply as a stringlist)
+        # see model/view programming
+        # http://doc.qt.nokia.com/latest/model-view-programming.html    
+        @artistModel = Qt::StringListModel.new
+        @albumModel = Qt::StringListModel.new
+        
 		# initialize the layout of the main widget
         layout = Qt::VBoxLayout.new(self) do |l|
-            l.add_widget(@artistList)
+            l.add_widget(@artistListView)
+            l.add_widget(@albumListView)
             l.add_widget(button)
         end
 		
@@ -28,23 +40,7 @@ class MainWidget < Qt::Widget
 			# attempt to connect to Ampache server
 			initializeConnection
 			
-			# artist model (defined simply as a stringlist)
-			# see model/view programming
-			# http://doc.qt.nokia.com/latest/model-view-programming.html
-			@artistModel = Qt::StringListModel.new do |model|
-				artists = @ampache.artists
- 				names = []
-				# extract the name from AmpacheArtist
-				# TODO: don't lose the uid
- 				artists.each do |artist|
- 					names << artist.name
- 				end
-				# initialize the model
-				model.setStringList names
-			end
-			
-			# set the model to the view
-			@artistList.setModel @artistModel
+            updateArtists
 		end
     end
 	
@@ -57,4 +53,40 @@ class MainWidget < Qt::Widget
 		
 		@ampache =AmpacheRuby.new(ar_config.get_value('AMPACHE_HOST'), ar_config.get_value('AMPACHE_USER'), ar_config.get_value('AMPACHE_USER_PSW'))
 	end
+    
+    def updateArtists
+        artists = @ampache.artists
+        names = []
+        # extract the name from AmpacheArtist
+        # TODO: don't lose the uid
+        artists.each do |artist|
+            names << artist.name
+        end
+        # initialize the model
+        @artistModel.setStringList names
+        
+        # set the model to the view
+        @artistListView.setModel @artistModel
+    end
+    
+    def updateAlbums(index)
+        @albumListView.setModel Qt::StringListModel.new
+        Thread.new do
+            # Get the selected artist
+            # TODO: should return the AmpacheArtist instead of just a string containing the name
+            artist = @artistModel.data(index, Qt::DisplayRole).value
+
+            # TODO: this is highly inefficient
+            # Must avoid to call @ampache.artists again
+            albums = @ampache.albums(@ampache.artists(artist).first)
+            names=[]
+            
+            albums.each do |album|
+                names << album.name
+            end
+            # initialize the model
+            @albumModel.setStringList names
+            @albumListView.setModel @albumModel
+        end
+    end
 end
